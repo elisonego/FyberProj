d54abc10930c335fc490e58dccf820680937da3d
eli ubuntu local vm <elisonego@gmail.com>
2023-03-20 15:48:35 +0200

    new file:   README.md 	modified:   myapp.py 	new file:   requirements.txt 	new file:   templates/index.html 	new file:   templates/result.html 	first commit of the python project of fyber

---
+++ README.md
@@ -0,0 +1,6 @@
+This is my project.
+myapp.py is where the flask app is at.
+it was dockerized and sits on an ec2 machine.
+there are 2 ec2 machines, and a load balancer.
+in addition there are several monitoring tools on the machines
+
--- myapp.py
+++ myapp.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/search", methods=["POST"])
+def search():
+    title = request.form.get("movie_title")
+    print(f"Searching for movie: {title}")
+    data = search_movie(title)
+    return render_template("result.html", data=data)
+
+def search_movie(title):
+    # Replace YOUR_API_KEY with your actual API key
+    api_key = "53ba70e1"
+    url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        # The API returns a JSON response, so we can use the built-in JSON parser
+        return response.json()
+    else:
+        # If there was an error with the API request, raise an exception
+        raise Exception("Unable to search movie")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
