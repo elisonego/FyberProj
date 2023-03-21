8941150bea82af7fd9ce3178902fbf6a563d7244
Ubuntu <ubuntu@ip-172-31-31-120.ec2.internal>
2023-03-21 12:16:00 +0000

    changes made to result.html old result.html is now called backup_res

--- myapp.py
+++ myapp.py
@@ -27,5 +27,5 @@ def search_movie(title):
         raise Exception("Unable to search movie")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port =5000)
 
