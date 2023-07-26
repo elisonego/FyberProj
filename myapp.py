d8e43eaf0bf2a94516ba2cc317fb431fefa4a309
elisonego <113848756+elisonego@users.noreply.github.com>
2023-07-27 01:43:47 +0300

    Update myapp.py
    last version

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ import requests
 import os
 import mysql.connector
 import json
-from datetime import datetime  # Add this line
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -19,46 +19,48 @@ def search():
     return render_template("result.html", data=data)
 
 def search_movie(title):
-    # Replace YOUR_API_KEY with your actual API key
-    api_key = os.getenv("OMDB_API_KEY")
-    url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        # Parse the release date
-        release_date = None
-        if 'Released' in data and data['Released']:
-            release_date = datetime.strptime(data['Released'], "%d %b %Y").date()
+    cnx = mysql.connector.connect(
+        host="dev-rds-instance.cq5fbtzsgyiw.us-east-1.rds.amazonaws.com",
+        user="elisonego",
+        password="adminADMIN123",
+        database="applicationDB"
+    )
 
-        # Connect to the database
-        cnx = mysql.connector.connect(
-            host="dev-rds-instance.cq5fbtzsgyiw.us-east-1.rds.amazonaws.com",
-            user="elisonego",
-            password="adminADMIN123",
-            database="applicationDB"
-        )
+    cursor = cnx.cursor()
 
-        cursor = cnx.cursor()
+    # Try to get the movie from the database
+    sql = ("SELECT data FROM movie_cache WHERE title = %s")
+    cursor.execute(sql, (title,))
+    result = cursor.fetchone()
 
-        # Insert the data into the database
-        sql = ("INSERT INTO movie_cache "
-               "(title, year, rated, released, data) "
-               "VALUES (%s, %s, %s, %s, %s)")
-
-        cursor.execute(sql, (title, data.get('Year', None), data.get('Rated', None), release_date, json.dumps(data)))
-
-        cnx.commit()
-
-        cursor.close()
-        cnx.close()
-        
-        return data
+    if result:
+        print("Movie found in cache")
+        data = json.loads(result[0])
     else:
-        # If there was an error with the API request, print the status code and response text
-        print(f"Status code: {response.status_code}")
-        print(f"Response text: {response.text}")
-        raise Exception("Unable to search movie")
+        # Movie not in cache, get it from the OMDB API
+        print("Movie not found in cache, fetching from API")
+        api_key = os.getenv("OMDB_API_KEY")
+        url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            # Store the movie in the database
+            sql = ("INSERT INTO movie_cache (title, year, runtime, genre, data) "
+                   "VALUES (%s, %s, %s, %s, %s)")
+            cursor.execute(sql, (title, data.get('Year', None), data.get('Runtime', None), data.get('Genre', None), json.dumps(data)))
+
+            cnx.commit()
+        else:
+            print(f"Status code: {response.status_code}")
+            print(f"Response text: {response.text}")
+            raise Exception("Unable to search movie")
+
+    cursor.close()
+    cnx.close()
+    
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port =5000)
-
