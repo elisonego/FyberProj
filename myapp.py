cdec26ff06184222db6d44103d36c01b743b0d4d
elisonego <113848756+elisonego@users.noreply.github.com>
2023-07-26 17:51:18 +0300

    Update myapp.py
    working and writing into the dabase

--- myapp.py
+++ myapp.py
@@ -1,35 +1,9 @@
-# from flask import Flask, render_template, request
-# import requests
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/search", methods=["POST"])
-# def search():
-#     title = request.form.get("movie_title")
-#     print(f"Searching for movie: {title}")
-#     data = search_movie(title)
-#     return render_template("result.html", data=data)
-
-# def search_movie(title):
-#     # Replace YOUR_API_KEY with your actual API key
-#     url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         # The API returns a JSON response, so we can use the built-in JSON parser
-#         return response.json()
-#     else:
-#         # If there was an error with the API request, raise an exception
-#         raise Exception("Unable to search movie")
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port =5000)
 from flask import Flask, render_template, request
 import requests
 import os
+import mysql.connector
+import json
+from datetime import datetime  # Add this line
 
 app = Flask(__name__)
 
@@ -50,13 +24,41 @@ def search_movie(title):
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
     response = requests.get(url)
     if response.status_code == 200:
-        # The API returns a JSON response, so we can use the built-in JSON parser
-        return response.json()
+        data = response.json()
+        # Parse the release date
+        release_date = None
+        if 'Released' in data and data['Released']:
+            release_date = datetime.strptime(data['Released'], "%d %b %Y").date()
+
+        # Connect to the database
+        cnx = mysql.connector.connect(
+            host="dev-rds-instance.cq5fbtzsgyiw.us-east-1.rds.amazonaws.com",
+            user="elisonego",
+            password="adminADMIN123",
+            database="applicationDB"
+        )
+
+        cursor = cnx.cursor()
+
+        # Insert the data into the database
+        sql = ("INSERT INTO movies_cache "
+               "(title, year, rated, released, data) "
+               "VALUES (%s, %s, %s, %s, %s)")
+
+        cursor.execute(sql, (title, data.get('Year', None), data.get('Rated', None), release_date, json.dumps(data)))
+
+        cnx.commit()
+
+        cursor.close()
+        cnx.close()
+        
+        return data
     else:
-        # If there was an error with the API request, raise an exception
+        # If there was an error with the API request, print the status code and response text
+        print(f"Status code: {response.status_code}")
+        print(f"Response text: {response.text}")
         raise Exception("Unable to search movie")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port =5000)
-
 
